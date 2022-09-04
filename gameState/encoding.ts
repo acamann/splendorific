18e4c656bc8c4231db94205c476ec27ee99f9a19
acamann <57601245+acamann@users.noreply.github.com>
2022-09-03 22:59:51 -0500

    encode initial deck state into game log (#18)
    * encode initial deck state into game log
    
    * comment

---
+++ gameState/encoding.ts
@@ -0,0 +1,9 @@
+import { Decks, Noble } from "../models";
+
+export const encodeInitialDecks = (decks: Decks, nobles: Noble[]): string => {
+  const encodedDecks = [1, 2, 3].map(level => `${level}:${decks[level as 1 | 2 | 3].map(d => d.id).join(",")}`).join("|");
+  const encodedNobles = `n:${nobles.map(n => n.id).join(",")}`;
+  return `${encodedDecks}|${encodedNobles}`;
+}
+
+// TODO: relocate other encoding logic here (from log additions in gamestate)
