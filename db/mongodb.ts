dfc6d37f69534975aa352e58dec2e6d067cb499b
acamann <57601245+acamann@users.noreply.github.com>
2022-09-08 20:52:09 -0500

    22 ongoing simulation runner (#26)
    * baseline endpoint simulates 20 different player experience configurations in one run
    
    * save baseline simulation to mongodb

--- db/mongodb.ts
+++ db/mongodb.ts
@@ -13,12 +13,29 @@ const saveDocument = async (collection: string, document: {}) => {
   await client.close();
 }
 
-export const saveSimulationToDB = async (simulationData: {}) =>
+const saveDocuments = async (collection: string, documents: {}[]) => {
+  await client.connect();
+  const collectionClient = client.db(database).collection(collection);
+  await collectionClient.insertMany(documents);
+  await client.close();
+}
+
+export const saveSimulationToDB = async (simulation: {}) =>
   await saveDocument("Simulations", {
-    ...simulationData,
+    ...simulation,
     timestamp: new Date().getTime(),
     version
   });
+
+
+export const saveSimulationsToDB = async (simulations: {}[]) =>
+  await saveDocuments("Simulations", simulations.map(data => {
+    return {
+      ...data,
+      timestamp: new Date().getTime(),
+      version
+    }
+  }));
 
 export const saveGameToDB = async (gameData: {}) =>
   await saveDocument("Games", {
