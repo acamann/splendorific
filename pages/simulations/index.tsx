79fb3044720ff4c842e797e0360c6f766554def8
Andy Camann <acamann@gmail.com>
2022-09-13 21:25:09 -0500

    a little more styling, still basic

--- pages/simulations/index.tsx
+++ pages/simulations/index.tsx
@@ -17,22 +17,23 @@ const Simulations: NextPage<SimulationPageProps> = ({ simulationQueue, simulatio
       </Head>
       <main style={{ display: "flex", gap: 24, padding: 24 }}>
         <div style={{ flexBasis: "25%", background: "white", padding: 16, maxHeight: "90vh" }}>
-          <h1 style={{ padding: 0, margin : 0 }}>Simulation Queue</h1>
-          <ol style={{ overflowY: "auto", maxHeight: "90%" }}>
+          <h2 style={{ padding: 0, margin : 0 }}>Simulation Queue ({simulationQueue.length})</h2>
+          <ol style={{ overflowY: "auto", maxHeight: "90%", margin: 0, padding: 0 }}>
             {simulationQueue.map((s, i) => 
-              <li key={i}>
-                {`${s.games} games: ${s.players.map(p => p.aiExperience).join(" vs ")}`}
+              <li key={i} style={{ padding: "8px 0", listStyle: "none", margin: 0 }}>
+                {`${s.games} games: ${s.players.map(p => Math.round(p.aiExperience * 100)).join(" vs ")}`}
               </li>
             )}
           </ol>
         </div>
         <div style={{ flexBasis: "75%", background: "white", padding: 16, maxHeight: "90vh" }}>
-          <h1 style={{ padding: 0, margin : 0 }}>Simulation Results</h1>
-          <ol style={{ overflowY: "auto", maxHeight: "90%" }}>
+          <h2 style={{ padding: 0, margin : 0 }}>Simulation Results</h2>
+          <ol style={{ overflowY: "auto", maxHeight: "90%", margin: 0, padding: 0 }}>
             {simulations.map((s, i) => 
-              <li key={i}>
-                <div>{`${s.games} games, ${s.players.length} players.`}</div>
-                {s.players.map((p, i) => <div key={i}>{`Experience Level ${Math.round(p.experience * 100)} => ${Math.round(p.winPercentage * 100)}%`}</div>)}
+              <li key={i} style={{ padding: "8px 0", listStyle: "none", margin: 0, display: "flex", gap: 24, fontSize: "0.9em" }}>
+                <div>{`${new Date(s.timestamp).toLocaleString()}`}</div>
+                <div>{`${s.games} games, ${s.players.length} players`}</div>
+                {s.players.map((p, i) => <div key={i}>{`${Math.round(p.experience * 100)} => ${Math.round(p.winPercentage * 100)}%`}</div>)}
               </li>
             )}
           </ol>
